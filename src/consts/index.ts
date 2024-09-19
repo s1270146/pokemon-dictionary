9407c704dafeeb6345db794a7752bf10239823f5
Koyo Mori <s1270146@gmail.com>
2024-09-19 21:27:42 +0900

    feat: :triangular_flag_on_post: #6 create heading and slide button

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -2,3 +2,7 @@
 export const logoPath: string = "/images/header/logo.svg";
 export const sortImagePath: string = "/images/header/icon_sort.svg";
 export const searchImagePath: string = "/images/header/icon_search.svg";
+
+// Heading
+
+export const pickupPath: string = "/images/heading/pickup.svg";
