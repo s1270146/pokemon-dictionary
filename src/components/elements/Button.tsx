89f76a6ee75735b8d9bce9c8384b1d0f57c21b80
Koyo Mori <s1270146@gmail.com>
2024-10-03 20:48:43 +0900

    feat: :triangular_flag_on_post: #9 create common button

---
+++ src/components/elements/Button.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from "react";
+
+interface ButtonProps {
+    isSelected: boolean;
+    onClick: ()=>void;
+    children?: ReactNode;
+    isDecision?: boolean;
+    width?: string;
+    height?: string;
+}
+
+export default function Button({
+        onClick,
+        isSelected=false,
+        children,
+        isDecision=false,
+        width="200px",
+        height="50px"
+    }: ButtonProps) {
+    return (
+        <div 
+            style={{
+                width,
+                height,
+            }}
+        >
+            <div 
+                className={`
+                    w-full h-full rounded-full
+                    ${isDecision ? `bg-red-900` : `bg-gray-300`}
+                `}
+            >
+                <button
+                    onClick={()=>onClick()}
+                    className={`
+                        px-4 py-2 w-full h-full text-black rounded-full border-2
+                        transform transition
+                        ${isSelected && !isDecision
+                            ? 'bg-yellow-50 border-yellow-500' 
+                            : 'bg-white -translate-x-1 -translate-y-1 border-gray-300 hover:translate-x-0 hover:translate-y-0'
+                        }
+                        ${isDecision && `bg-red-600 border-red-600`}
+                    `}
+                    >
+                        {children}
+                </button>
+            </div>
+        </div>
+    );
+}
