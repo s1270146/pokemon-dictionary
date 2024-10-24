6160a02ab7de5d21120e8d4e2928881d6e0808d6
Koyo Mori <s1270146@gmail.com>
2024-10-24 21:05:22 +0900

    fix: #13 modify search modal

--- src/features/common/search/components/SearchButton.tsx
+++ src/features/common/search/components/SearchButton.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Link from "next/link";
 import Image from "next/image";
 import { searchImagePath } from "@/consts";
 import SearchModal from "./SearchModal";
@@ -10,12 +9,12 @@ export default function SearchButton (){
     const {isOpen, openModal, closeModal} = useSearchModal();
 
     return <>
-        <Link href="#" onClick={()=>openModal()} className="ml-2">
+        <div onClick={()=>{openModal()}} className="ml-2">
             <div className="h-[45px]">
                 <Image src={searchImagePath} alt="検索" height={40} width={40}/>
             </div>
             <p className="text-xs">検索</p>
-        </Link>
+        </div>
         <SearchModal isOpen={isOpen} closeModal={()=>closeModal()}/>
     </>
 }
