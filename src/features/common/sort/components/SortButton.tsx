6a9fac988be430e98bea13694e5033759fac541c
Koyo Mori <s1270146@gmail.com>
2024-10-24 21:03:39 +0900

    feat: #15 create sort modal

--- src/features/common/sort/components/SortButton.tsx
+++ src/features/common/sort/components/SortButton.tsx
@@ -1,12 +1,22 @@
-import Link from "next/link";
+"use client"
+
 import Image from "next/image";
 import { sortImagePath } from "@/consts";
+import SortModal from "./SortModal";
+import { useSortModal } from "../hooks";
 
 export default function SortButton(){
-    return <Link href="#" className="ml-2">
-        <div className="h-[45px]">
-            <Image src={sortImagePath} alt="表示順" width={50} height={38}/>
-        </div>
-        <p className="text-xs">表示順</p>
-    </Link>
+    const {isOpen, openModal, closeModal} = useSortModal();
+
+    return (
+        <>
+            <div onClick={()=>{openModal()}} className="ml-2">
+                <div className="h-[45px]">
+                    <Image src={sortImagePath} alt="表示順" width={50} height={38}/>
+                </div>
+                <p className="text-xs">表示順</p>
+            </div>
+            <SortModal isOpen={isOpen} closeModal={closeModal}/>
+        </>
+    );
 }
